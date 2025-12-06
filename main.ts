4025e1b50880c883e6b6f9ce01b4af329921a680
asuka <yagi.asuka.0x06@gmail.com>
2025-12-06 20:07:41 +0900

    feat: update profile

--- main.ts
+++ main.ts
@@ -1,2 +1,55 @@
-import { green } from "@std/fmt/colors";
-console.log(green("Hello, World!"));
+import { gray, green, italic } from "@std/fmt/colors";
+
+export const me: string = `
+${italic("name")} = "${green("asuka")}"
+${italic("role")} = "${green("SWE")}"
+${italic("lang")} = "${green("ja")}"
+${italic("loc")}  = "${green("Japan")}"
+${italic("fav")}  = "${green("WebAssembly")}"
+${italic("org")}  = "${green("株式会社モニクル")}"
+
+${gray("[sns]")}
+${italic("X")}       = "${green("@a_skua")}"
+${italic("GitHub")}  = "${green("@a-skua")}"
+${italic("Bruesky")} = "${green("@askua.dev")}"
+
+${gray("[[book]]")}
+${italic("title")}   = "${green("WebAssemblyでできること")}"
+${italic("publish")} = "${green("技術書典14")}"
+
+${gray("[[book]]")}
+${italic("title")}   = "${green("実践入門WebAssembly")}"
+${italic("publish")} = "${green("インプレス NextPublishing")}"
+
+${gray("[[book]]")}
+${italic("title")}   = "${green("ご注文はWASIですか?")}"
+${italic("publish")} = "${green("技術書典15")}"
+
+${gray("[[book]]")}
+${italic("title")}   = "${green("ご注文はWASIですか??")}"
+${italic("publish")} = "${green("技術書典16")}"
+
+${gray("[[book]]")}
+${italic("title")}   = "${green("WebAssembly Cookbook vol.1")}"
+${italic("publish")} = "${green("技術書典16")}"
+
+${gray("[[book]]")}
+${italic("title")}   = "${green("WebAssembly System Interface入門")}"
+${italic("publish")} = "${green("インプレス NextPublishing")}"
+
+${gray("[[book]]")}
+${italic("title")}   = "${green("WebAssembly Cookbook vol.2")}"
+${italic("publish")} = "${green("技術書典17")}"
+
+${gray("[[book]]")}
+${italic("title")}   = "${green("WebAssembly Cookbook vol.3")}"
+${italic("publish")} = "${green("技術書典18")}"
+
+${gray("[[book]]")}
+${italic("title")}   = "${green("WebAssembly Cookbook vol.4")}"
+${italic("publish")} = "${green("技術書典19")}"
+`.trim();
+
+if (import.meta.main) {
+  console.log(me);
+}
