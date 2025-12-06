c53e4fd3422c8d53f07b8962cb7e2d410294e37f
asuka <yagi.asuka.0x06@gmail.com>
2025-12-07 02:47:03 +0900

    fix

--- README.md
+++ README.md
@@ -2,6 +2,34 @@
 
 ![CI](https://github.com/a-skua/whois.js/workflows/Publish/badge.svg)
 
+## CLI
 ```sh
 deno run jsr:@askua/whois
+```
+
+## Browser
+```html
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>WHOIS ME</title>
+  <script type="importmap">
+    {
+      "imports": {
+        "@askua/whois": "https://esm.sh/jsr/@askua/whois@2025.12.6"
+      }
+    }
+  </script>
+  <script type="module">
+    import { me } from "@askua/whois";
+    console.log(me);
+  </script>
+</head>
+<body>WHOIS ME</body>
+</html>
+```
+
+```sh
+deno run --allow-net --allow-read jsr:@std/http/file-server
 ```
--- main.ts
+++ main.ts
@@ -17,39 +17,39 @@ ${italic("Bruesky")} = "${green("@askua.dev")}"
 
 ${gray("[[book]]")}
 ${italic("title")}   = "${green("WebAssemblyでできること")}"
-${italic("publish")} = "${green("技術書典14")}"
+${italic("publish")} = "技術書典14"
 
 ${gray("[[book]]")}
 ${italic("title")}   = "${green("実践入門WebAssembly")}"
-${italic("publish")} = "${green("インプレス NextPublishing")}"
+${italic("publish")} = "インプレス NextPublishing"
 
 ${gray("[[book]]")}
 ${italic("title")}   = "${green("ご注文はWASIですか?")}"
-${italic("publish")} = "${green("技術書典15")}"
+${italic("publish")} = "技術書典15"
 
 ${gray("[[book]]")}
 ${italic("title")}   = "${green("ご注文はWASIですか??")}"
-${italic("publish")} = "${green("技術書典16")}"
+${italic("publish")} = "技術書典16"
 
 ${gray("[[book]]")}
 ${italic("title")}   = "${green("WebAssembly Cookbook vol.1")}"
-${italic("publish")} = "${green("技術書典16")}"
+${italic("publish")} = "技術書典16"
 
 ${gray("[[book]]")}
 ${italic("title")}   = "${green("WebAssembly System Interface入門")}"
-${italic("publish")} = "${green("インプレス NextPublishing")}"
+${italic("publish")} = "インプレス NextPublishing"
 
 ${gray("[[book]]")}
 ${italic("title")}   = "${green("WebAssembly Cookbook vol.2")}"
-${italic("publish")} = "${green("技術書典17")}"
+${italic("publish")} = "技術書典17"
 
 ${gray("[[book]]")}
 ${italic("title")}   = "${green("WebAssembly Cookbook vol.3")}"
-${italic("publish")} = "${green("技術書典18")}"
+${italic("publish")} = "技術書典18"
 
 ${gray("[[book]]")}
 ${italic("title")}   = "${green("WebAssembly Cookbook vol.4")}"
-${italic("publish")} = "${green("技術書典19")}"
+${italic("publish")} = "技術書典19"
 `.trim();
 
 if (import.meta.main) {
